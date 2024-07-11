7c74554cd952e32244b640158b77511f0c58fb8d
Aurimas <154673326+darbai123@users.noreply.github.com>
2024-07-12 00:43:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -34,3 +34,46 @@ function closeModal(modal) {
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+
+// ----------------------SLIDE SHOW------------------------------------------------------------
+
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+let itemWidth = items[0].offsetWidth; // Pasiimame vieno elemento plotį
+
+next.addEventListener('click', function () {
+    active = (active + 1) % items.length;
+    reloadSlider();
+});
+
+prev.addEventListener('click', function () {
+    active = (active - 1 + items.length) % items.length;
+    reloadSlider();
+});
+
+let refreshSlider = setInterval(() => { next.click() }, 10000); // Pakeisti slankiklio atnaujinimo dažnumą pagal poreikį
+
+function reloadSlider() {
+    let checkLeft = -active * itemWidth; // Pritaikome vieno elemento plotį
+    list.style.transform = `translateX(${checkLeft}px)`;
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function () {
+        active = key;
+        reloadSlider();
+    });
+});
+
+
